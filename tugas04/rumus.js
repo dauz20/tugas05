651e451fdea3b22b2711ac47035a60877926baeb
dauz20 <72052839+dauz20@users.noreply.github.com>
2025-03-26 21:46:23 +0700

    Add files via upload
    penambahan tugas 04 file menu.js & rumus.js

---
+++ tugas04/rumus.js
@@ -0,0 +1,16 @@
+export function kalkulator(angka1, angka2, operator) {
+    switch (operator) {
+        case "+":
+            return angka1 + angka2;
+        case "-":
+            return angka1 - angka2;
+        case "*":
+            return angka1 * angka2;
+        case "/":
+            return angka2 !== 0 ? angka1 / angka2 : "Tidak dapat membagi dengan nol!";
+        default:
+            return "Operator tidak valid!";
+    }
+}
+
+
