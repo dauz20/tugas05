ec48b0ab7d5b9e406928a6f10faf27fbc89db45f
dauz20 <72052839+dauz20@users.noreply.github.com>
2025-03-26 21:45:01 +0700

    Add files via upload
    penambahan tugas 03 file "sesi3_firdausnuzula.js"

---
+++ tugas03/sesi3_firdausnuzula.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 5; i++) {
+    let bintang = ""; // Inisialisasi string kosong
+    for (let j = 1; j <= i; j++) {
+      bintang += "*"; // Tambahkan bintang ke string
+    }
+    console.log(bintang); // Cetak string pada setiap iterasi
+  }
+  
+  console.log("Hello world");
